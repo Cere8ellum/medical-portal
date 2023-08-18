2d1a5d480b29c017ea031d626bee273b447512de
Gena <mr.genag@mail.ru>
2023-08-18 19:39:08 +0500

    Sprint_3 Сделал страничку смены пароля и прописал роут для неё

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -4,6 +4,7 @@ import MainScreen from '../pages/main';
 import AppointmentScreen from '../pages/appointment';
 import { Profile } from '../pages/profile/components/Profile';
 import { SignupScreen } from '../pages/signup';
+import ChangePassword from '../pages/changepassword/ChangePassword';
 
 export function App() {
   return (
@@ -12,6 +13,7 @@ export function App() {
       <Route path="/signup" element={<SignupScreen />} />
       <Route path="/appointment" element={<AppointmentScreen />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/changepassword" element={<ChangePassword />} />
     </Routes>
   );
 }
