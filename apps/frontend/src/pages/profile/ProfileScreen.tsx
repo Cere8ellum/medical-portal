d33a14ccec8ec161f6c2e6552bc02036f42bc013
Roman Kobeev <67678390+romank123@users.noreply.github.com>
2023-08-21 09:53:54 +0300

    feta: переключающиеся табы в лк пациента

--- apps/frontend/src/pages/profile/ProfileScreen.tsx
+++ apps/frontend/src/pages/profile/ProfileScreen.tsx
@@ -1,4 +1,4 @@
-import MainLayout from 'apps/frontend/src/app/MainLayout';
+import MainLayout from 'apps/frontend/src/app/MainLayout/';
 import { useEffect, useState } from 'react';
 import styles from './styles/profile.module.css';
 import axios from 'axios';
@@ -7,6 +7,10 @@ import PersonalDataField from './components/PersonalDataField';
 import PersonalDataButton from './components/PersonalDataButton';
 
 function ProfileScreen() {
+  const { idx, Tabs } = TabsPersonalAccountPatient();
+  useEffect(() => {
+    getUser();
+  }, []);
   async function getUser() {
     try {
       const response = await axios.get('http://localhost:3000/api/user/1');
@@ -16,21 +20,28 @@ function ProfileScreen() {
     }
   }
 
-  useEffect(() => {
-    getUser;
-  }, []);
-
-  return (
-    <MainLayout>
-      <main className={styles['content']}>
-        <TabsPersonalAccountPatient />
-
+  function ActiveTab() {
+    if (idx === 1)
+      return (
         <div className={styles['content-form']}>
           <form className={styles['persdata-form']} action="#">
             <PersonalDataField />
             <PersonalDataButton />
           </form>
         </div>
+      );
+    else if (idx === 2) {
+      return <div>информация о записи</div>;
+    } else if (idx === 3) {
+      return <div>медицинская история</div>;
+    }
+  }
+
+  return (
+    <MainLayout>
+      <main className={styles['content']}>
+        {Tabs}
+        <ActiveTab></ActiveTab>
       </main>
     </MainLayout>
   );
--- apps/frontend/src/pages/profile/components/TabsPersonalAccountPatient.tsx
+++ apps/frontend/src/pages/profile/components/TabsPersonalAccountPatient.tsx
@@ -1,37 +1,67 @@
-import styles from '../styles/profile.module.css';
+import { useState, useEffect } from 'react';
+import styles from '../styles/profile.module.scss';
 
+let idx = 1;
 function TabsPersonalAccountPatient() {
-  return (
-    <div className={styles['content-top']}>
-      <ul className={styles['content-nav']}>
-        <li
-          className={`${styles['content-nav-item']} ${styles['content-nav-item-active']}`}
-        >
-          <button
-            className={`${styles['content-nav-btn']} ${styles['content-nav-btn-persdata']}`}
-          >
-            Личные данные
-          </button>
-        </li>
-        <li className={styles['content-nav-item']}>
-          <button
-            className={`${styles['content-nav-btn']} ${styles['content-nav-btn-infovisit']}`}
-          >
-            Информация о записи
-          </button>
-        </li>
-        <li className={styles['content-nav-item']}>
-          <button
-            className={`${styles['content-nav-btn']} ${styles['content-nav-btn-medicalhistory']}`}
-          >
-            Медицинская история
-          </button>
-        </li>
-      </ul>
+  const [activeIndex, setActiveIndex] = useState(1);
+  const handleClick = (index: number) => {
+    setActiveIndex(index);
+    idx = index;
+    return index;
+  };
+  const checkActive = (index: number, className: string) =>
+    activeIndex === index ? className : '';
 
-      <h3 className={styles['content-nav-title']}>Личный кабинет</h3>
-    </div>
-  );
+  return {
+    idx,
+    Tabs: (
+      <div className={styles['content-top']}>
+        <ul className={styles['content-nav']}>
+          <li
+            className={`${styles['content-nav-item']} ${checkActive(
+              1,
+              styles['active']
+            )}`}
+            onClick={() => handleClick(1)}
+          >
+            <button
+              className={`${styles['content-nav-btn']} ${styles['content-nav-btn-persdata']}`}
+            >
+              Личные данные
+            </button>
+          </li>
+          <li
+            className={`${styles['content-nav-item']} ${checkActive(
+              2,
+              styles['active']
+            )}`}
+            onClick={() => handleClick(2)}
+          >
+            <button
+              className={`${styles['content-nav-btn']} ${styles['content-nav-btn-infovisit']}`}
+            >
+              Информация о записи
+            </button>
+          </li>
+          <li
+            className={`${styles['content-nav-item']} ${checkActive(
+              3,
+              styles['active']
+            )}`}
+            onClick={() => handleClick(3)}
+          >
+            <button
+              className={`${styles['content-nav-btn']} ${styles['content-nav-btn-medicalhistory']}`}
+            >
+              Медицинская история
+            </button>
+          </li>
+        </ul>
+
+        <h3 className={styles['content-nav-title']}>Личный кабинет</h3>
+      </div>
+    ),
+  };
 }
 
 export default TabsPersonalAccountPatient;
