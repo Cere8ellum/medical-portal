f7d99a4d0bae252cae8166417ff4969a1a1bba8f
Gena <mr.genag@mail.ru>
2023-09-06 18:03:56 +0500

    #gg-sprint6 Перенёс стили со страницы ЛК-Доктора

---
+++ apps/frontend/src/pages/doctor_personal_office/DoctorPersonalOffice.tsx
@@ -0,0 +1,7 @@
+import '../../pages/doctor_personal_office/styles/style.css';
+
+const DoctorPersonalOffice = () => {
+  return <div className="jkl">hhhh</div>;
+};
+
+export default DoctorPersonalOffice;
